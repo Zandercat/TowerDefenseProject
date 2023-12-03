01f7e08c02008b7b4bdd1a93597e6bf4e943aa36
KatopoJo <jokatopo.15@gmail.com>
2023-12-03 13:18:54 -0500

    Added enemy health bar functionality

--- Assets/Art/scripts/Health.cs
+++ Assets/Art/scripts/Health.cs
@@ -6,13 +6,24 @@ public class Health : MonoBehaviour
 {
     [Header("Attributes")]
     public int hitPoints = 2;
+    public int MaxHitPoints;
     public int[] damageResistances = new int[3]; //Element 0 is damage from DPS. Element 1 is damage from AoE. Element 2 is damage from Support.
     //these are percent resistances. 0 is no resistance. 100 is immunity. >100 will cause healing from damage.
+
+    public EnemyHealthBarScript healthBar;
+
+    void Awake()
+    {
+        MaxHitPoints = hitPoints;
+        healthBar.SetMaxHealth(MaxHitPoints);
+    }
 
     public void TakeDamage(int dmg, int dmgType)
     {
         int newDmg = dmg * (100 - damageResistances[dmgType]) / 100; //integer division, round down to final hp number
         hitPoints -= newDmg;
+
+        healthBar.SetHealth(hitPoints);
 
         if(hitPoints <= 0)
         {
