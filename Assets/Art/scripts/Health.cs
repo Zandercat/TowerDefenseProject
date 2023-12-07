2f5c6818e278ed23d40b832fd08468940f16a15d
Zandercat <42007489+Zandercat@users.noreply.github.com>
2023-12-07 17:39:53 -0500

    money drops

--- Assets/Art/scripts/Health.cs
+++ Assets/Art/scripts/Health.cs
@@ -9,6 +9,7 @@ public class Health : MonoBehaviour
     public int MaxHitPoints;
     public int[] damageResistances = new int[3]; //Element 0 is damage from DPS. Element 1 is damage from AoE. Element 2 is damage from Support.
     //these are percent resistances. 0 is no resistance. 100 is immunity. >100 will cause healing from damage.
+    public int moneyDropped;
 
     public HealthBarScript healthBar;
 
@@ -27,6 +28,7 @@ public class Health : MonoBehaviour
 
         if(hitPoints <= 0)
         {
+            GameObject.Find("Canvas").GetComponent<UIScript>().gainMoney(moneyDropped);
             enermySpawner.onEnemyDestroy.Invoke();
             Destroy(gameObject);
         }
